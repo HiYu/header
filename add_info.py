6e5b5d54474eefc623b1a0dc43fbfb3bb04b9d3d
Frank <zhaoyyuu@gmial.com>
2016-01-25 13:04:25 +0800

    本程序一个简单的sublime text 3插件，用于给代码添加头部信息，目前支持python，PHP，C/C++，其他语言可根据需要按照已有格式修改代码。

---
+++ add_info.py
@@ -0,0 +1,31 @@
+import datetime
+import sublime, sublime_plugin
+
+class AddPythonHeaderCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.run_command("insert_snippet",
+             {
+               "contents":
+                "# Copyright (C) 2016-2017 ISCAS""\n"
+                "# Author:      Frank""\n"
+                "# DateTime:    "  "%s"  %datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") +"\n"
+                "# Contact: yuhai@nfs.iscas.ac.cn""\n"
+                "# Description: ${1:Description}""\n"
+            }
+        )
+
+class AddPhpHeaderCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.run_command("insert_snippet",
+            {
+               "contents":"/**""\n"
+                "* Copyright (C) 2016-2017 ISCAS""\n"
+                "* Author:      Frank""\n"
+                "* DateTime:    "  "%s"  %datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") +"\n"
+                "* Contact: yuhai@nfs.iscas.ac.cn""\n"
+                "* Description: ${1:Description}""\n"
+                "**/""\n"
+            }
+        )
+
+
